64594162c4fcb3c9a3ead4413f56ff9ba2cbe2f3
tkorays <tkorays@hotmail.com>
2014-03-14 15:42:09 +0800

    一个简单的程序

---
+++ gray/GrayImageForm.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace gray {
+    partial class GrayImage {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent() {
+            this.Open = new System.Windows.Forms.Button();
+            this.SaveBtn = new System.Windows.Forms.Button();
+            this.CloseBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(37, 32);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(75, 23);
+            this.Open.TabIndex = 0;
+            this.Open.Text = "打开文件";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.OpenBtn_Click);
+            // 
+            // SaveBtn
+            // 
+            this.SaveBtn.Location = new System.Drawing.Point(37, 71);
+            this.SaveBtn.Name = "SaveBtn";
+            this.SaveBtn.Size = new System.Drawing.Size(75, 23);
+            this.SaveBtn.TabIndex = 1;
+            this.SaveBtn.Text = "保存图像";
+            this.SaveBtn.UseVisualStyleBackColor = true;
+            this.SaveBtn.Click += new System.EventHandler(this.SaveBtn_Click);
+            // 
+            // CloseBtn
+            // 
+            this.CloseBtn.Location = new System.Drawing.Point(37, 109);
+            this.CloseBtn.Name = "CloseBtn";
+            this.CloseBtn.Size = new System.Drawing.Size(75, 23);
+            this.CloseBtn.TabIndex = 2;
+            this.CloseBtn.Text = "关闭";
+            this.CloseBtn.UseVisualStyleBackColor = true;
+            this.CloseBtn.Click += new System.EventHandler(this.CloseBtn_Click);
+            // 
+            // GrayImage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 369);
+            this.Controls.Add(this.CloseBtn);
+            this.Controls.Add(this.SaveBtn);
+            this.Controls.Add(this.Open);
+            this.Name = "GrayImage";
+            this.Text = "GrayImage";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.Button SaveBtn;
+        private System.Windows.Forms.Button CloseBtn;
+    }
+}
+
---
+++ gray/GrayImageForm.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gray {
+    public partial class GrayImage : Form {
+        private string curFileName;
+        private System.Drawing.Bitmap curBitmap;
+        public GrayImage() {
+            InitializeComponent();
+        }
+
+        private void OpenBtn_Click(object sender, EventArgs e) {
+            OpenFileDialog opnDlg = new OpenFileDialog();
+            opnDlg.Filter = "所有图像文件|*.bmp;*.pcx;*.jpg;*.jpeg;*.gif;";
+            opnDlg.Title = "打开图像文件";
+            opnDlg.ShowHelp = true;
+            if (opnDlg.ShowDialog()==DialogResult.OK) {
+                curFileName = opnDlg.FileName;
+                try {
+                    curBitmap = (Bitmap)Image.FromFile(curFileName);
+                } catch (Exception exp) {
+                    MessageBox.Show(exp.Message);
+                }
+            }
+            Invalidate();
+        }
+
+        private void SaveBtn_Click(object sender, EventArgs e) {
+            if (curBitmap==null) {
+                return;
+            }
+            SaveFileDialog saveDlg = new SaveFileDialog();
+            saveDlg.Title = "保存为";
+            saveDlg.OverwritePrompt = true; // 改写已存在文件时提醒用户
+            saveDlg.Filter = "BMP文件 (*.bmp) | *.bmp|" + "Gif文件 (*.gif) | *.gif|"+"JPEG文件 (*.jpg) | *.jpg|"+"PNG文件 (*.png) | *.png";
+            saveDlg.ShowHelp = true;
+            if (saveDlg.ShowDialog()==DialogResult.OK) {
+                string fileName = saveDlg.FileName;
+                string strFiExtn = fileName.Remove(0, fileName.Length - 3);
+
+                switch (strFiExtn) {
+                    case "bmp":
+                        curBitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                        break;
+                    case "jpg":
+                        curBitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        break;
+                    case "png":
+                        curBitmap.Save(fileName, System.Drawing.Imaging.ImageFormat.Png);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        private void CloseBtn_Click(object sender, EventArgs e) {
+            this.Close();
+        }
+
+        protected override void OnPaint(PaintEventArgs e) {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+            if (curBitmap!=null) {
+                g.DrawImage(curBitmap, 160, 20, 300, 200);
+            }
+        }
+    }
+}
